8b25c686dec4784ae7db24e5c969af47d3de0caa
hameed Kazeem <hameed.kazeem@gmail.com>
2020-09-24 18:49:06 -0400

    add fail test for player and code to pass

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,4 +1,8 @@
+const { JestEnvironment } = require("@jest/environment");
 const Player = require("../lib/Player.js");
+const Potion = require("../lib/__mocks__/Potion.js");
+jest.mock('../lib/potion');
+console.log(new Potion());
 
 test('creates a player object', () => {
     const player = new Player('Dave');
@@ -7,5 +11,8 @@ test('creates a player object', () => {
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+      );
 
 });
